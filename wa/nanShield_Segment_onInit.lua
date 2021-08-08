e7a25d12e647bc2fbb298d18cbb15f055a989cf0
Guido Schroeder <37477166+schroeg@users.noreply.github.com>
2021-08-08 15:34:08 +0200

    Fixed out of bounce index when playing arena

--- wa/nanShield_Segment_onInit.lua
+++ wa/nanShield_Segment_onInit.lua
@@ -43,7 +43,7 @@ function aura_env:on_tsu(allstates, ...)
             end
         end
         for i = 1, active do
-            if allstates[i].show then
+            if i <= #allstates and allstates[i].show then
                 if allstates[i].school ~= self.segmentSchool[i] then
                     self:log('TSUSchool', i, allstates[i].school, self.segmentSchool[i])
                     allstates[i].school = self.segmentSchool[i]
